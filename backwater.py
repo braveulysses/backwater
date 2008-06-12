5a2f882c84982f76bf603ba162776f1418fb8b5e
c.libre <jacob@spaceshipnofuture.org>
2008-06-12 05:05:46 +0000

    Added stubs for Sources and Entries
    git-svn-id: http://code.chompy.net/backwater@5 356b7e19-ef4e-0410-b57b-97b486cf1154

--- backwater.py
+++ backwater.py
@@ -12,12 +12,53 @@ __author__ = "SNF Labs <jacob@spaceshipnofuture.org>"
 
 import sys
 import getopt
-
+import yaml
 
 help_message = '''
 The help message goes here.
 '''
 
+#################################################
+
+# Sources (feeds) and subtypes of sources.
+
+class Source: pass
+
+class Weblog(Source): pass
+
+class Linklog(Source): pass
+
+class Tumblelog(Source): pass
+
+class Photostream(Source): pass
+
+class Twitter(Source): pass
+
+# Entries and subtypes of entries.
+
+class Entry: pass
+
+class Post(Entry): pass
+
+class Link(Entry): pass
+
+class Quote(Entry): pass
+
+class Song(Entry): pass
+
+class Video(Entry): pass
+
+class Photo(Entry): pass
+
+#################################################
+
+def get_configuration(config_file):
+    try:
+        f = open(config_file, 'r')
+        config = yaml.load(f)
+        return config
+    finally:
+        f.close()
 
 class Usage(Exception):
 	def __init__(self, msg):
@@ -44,8 +85,11 @@ def main(argv=None):
 				
 		# Okay, GO
 		# Read configuration
+		config = get_configuration('config.yaml')
+		print yaml.dump(config)
 		# Get feeds
 		# Get tumblelogs
+		# Get twitters
 		# Get photos
 		# Merge, sort, and collate
 		# Write output
