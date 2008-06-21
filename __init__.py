0e46915975b169f7970757ea40052b46d2dd299c
c.libre <jacob@spaceshipnofuture.org>
2008-06-21 15:53:42 +0000

    Make backwater more packagey
    git-svn-id: http://code.chompy.net/backwater@8 356b7e19-ef4e-0410-b57b-97b486cf1154

---
+++ __init__.py
@@ -0,0 +1,8 @@
+from backwater import Source
+from backwater import Weblog
+from backwater import Linklog
+from backwater import Tumblelog
+from backwater import Photostream
+from backwater import TwitterStatus
+
+from backwater import Entry
