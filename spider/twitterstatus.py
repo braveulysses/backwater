39f87556a5c74f1431889d4c4808262b5d7ccd76
c.libre <jacob@spaceshipnofuture.org>
2008-07-01 03:05:02 +0000

    Added some basic exception handling.
    git-svn-id: http://code.chompy.net/backwater@31 356b7e19-ef4e-0410-b57b-97b486cf1154

--- spider/twitterstatus.py
+++ spider/twitterstatus.py
@@ -7,6 +7,7 @@ Created by Jacob C. on 2008-06-22.
 Copyright (c) 2008 Spaceship No Future. All rights reserved.
 """
 
+import httplib
 import twitter
 import logging
 import config
@@ -28,17 +29,25 @@ class TwitterStatus(Source):
     def parse(self):
         """Fetches Twitter status using the Twitter API."""
         self.logger.debug("Contacting Twitter services")
-        twitty = twitter.Api(username=config.TWITTER_ACCOUNT, password=config.TWITTER_PASSWORD)
-        self.logger.info("Getting Twitter statuses for %s" % self.owner)
-        statuses = twitty.GetUserTimeline(self.name)
-        for status in statuses:
-            self.author = status.user.name
-            self.summary = status.text
-            self.content = self.summary
-            self.logger.info("Twitter: '%s'" % self.summary)
-            self.url = self.get_status_url(status.id)
-            self.logger.debug("Twitter URL: %s" % self.url)
-            self.date = status.created_at
+        try:
+            twitty = twitter.Api(
+                username=config.TWITTER_ACCOUNT, 
+                password=config.TWITTER_PASSWORD
+            )
+            self.logger.info("Getting Twitter statuses for %s" % self.owner)
+            statuses = twitty.GetUserTimeline(self.name)
+            for status in statuses:
+                self.author = status.user.name
+                self.summary = status.text
+                self.content = self.summary
+                self.logger.info("Twitter: '%s'" % self.summary)
+                self.url = self.get_status_url(status.id)
+                self.logger.debug("Twitter URL: %s" % self.url)
+                self.date = status.created_at
+        except httplib.BadStatusLine:
+            self.logger.exception("Twitter.com unexpectedly closed the connection!")
+        except:
+            self.logger.exception("Unknown error while fetching Twitter status!")
 
 def main():
     pass
