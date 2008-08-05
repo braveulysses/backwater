e122d82ed3bbca227f7a6c5af013c6788b8ce230
c.libre <jacob@spaceshipnofuture.org>
2008-08-05 04:25:02 +0000

    Removed tests/sanitizer because the sanitizer tests don't require any external files. Moved a bunch of junk into the util folder.
    git-svn-id: http://code.chompy.net/backwater@101 356b7e19-ef4e-0410-b57b-97b486cf1154

---
+++ util/backwaterfeeds.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+wget -O remakeremodel.xml http://chompy.net/blogs/jacob/feeds/index.atom
+wget -O darkentries.xml http://www.chompy.net/blogs/jacob/feeds/darkentries.atom
+wget -O peenywally.xml http://chompy.net/blogs/pogo/atom.xml
+wget -O tigerpounces.xml http://chompy.net/blogs/nathalie/atom.xml
+wget -O greencandle.xml http://palcontent.blogspot.com/feeds/posts/default
+wget -O garbage.xml http://hoolifan.blogspot.com/feeds/posts/default
+wget -O bubblegumdamage.xml http://bubblegumdamage.blogspot.com/feeds/posts/default
+wget -O charlieletters.xml http://savagepencil.typepad.com/charlieletters/index.rdf
+wget -O confeessions.xml http://savagepencil.typepad.com/confessions/index.rdf
+wget -O headcrab.xml http://headcrab.org/feed/atom/
+wget -O arabic.xml http://benkerishan.blogspot.com/feeds/posts/default
+wget -O ongoing.xml http://www.tbray.org/ongoing/ongoing.atom
+wget -O newsofthedead.xml http://wileywiggins.blogspot.com/feeds/posts/default
